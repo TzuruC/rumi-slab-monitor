bb7192690c4a53fef93faeca5f40f815390b5d64
Rumi <tzuruc@gmail.com>
2025-01-16 11:52:11 +0800

    惻選單切板完成

--- Plugin/js/asideMenu.js
+++ Plugin/js/asideMenu.js
@@ -1,10 +1,13 @@
-const toggleAsideBtn = document.querySelector('.SideMenu-SwitchBtn');
+const toggleAsideBtn = document.querySelector('#toggleAsideBtn');
+const toggleAsideMenu = document.querySelector('#toggleAsideMenu');
+const toggleContentWidth = document.querySelector('#toggleContentWidth');
 
 toggleAsideBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(toggleAsideBtn);
-    const childSubmenu = submenu.querySelector('.fa-solid');
-    if (childSubmenu) {
-        childSubmenu.classList.toggle('show');
+    const childIcon = toggleAsideBtn.querySelector('.fa-solid');
+    if (childIcon) {
+        childIcon.classList.toggle('fa-rotate-180');
     }
+    toggleAsideMenu.classList.toggle('aside-hide');
+    toggleContentWidth.classList.toggle('aside-open');
 });
