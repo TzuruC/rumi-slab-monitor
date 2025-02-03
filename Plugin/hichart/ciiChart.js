3cb1aa7d56c511b2f4abf75df79073a22f10b500
Rumi <tzuruc@gmail.com>
2025-02-03 10:37:50 +0800

    解決頁面上多個hichart衝突

--- Plugin/hichart/ciiChart.js
+++ Plugin/hichart/ciiChart.js
@@ -1,8 +1,4 @@
-const attainedCII = 0;
-const requiredCII = 0;
-const rateNY = ['A', 'B', 'C', 'D', 'E'];
-
-Highcharts.setOptions({
+const chartOptions = {
     chart: {
         inverted: true,
         marginLeft: 0,
@@ -17,8 +13,7 @@ Highcharts.setOptions({
     },
     yAxis: {
         gridLineWidth: 0,
-        max: 10, // 設定Y軸最大值 10
-        // 圖表高度在CSS設置
+        max: 10,
     },
     plotOptions: {
         series: {
@@ -39,37 +34,31 @@ Highcharts.setOptions({
     exporting: {
         enabled: false,
     },
-});
+};
 
 Highcharts.chart('containerCii', {
+    ...chartOptions, // 只影響這個圖表
     chart: {
+        ...chartOptions.chart,
         backgroundColor: '#323639',
         marginTop: 40,
     },
-    title: {
-        text: '',
-        color: '#fff',
-    },
     yAxis: [
         {
-            // 第一個 Y 軸
             plotBands: [
-                // 共用的區域背景
                 { from: 0, to: 2, color: '#57D637' },
                 { from: 2, to: 3, color: '#94BF3B' },
                 { from: 3, to: 7, color: '#C6C918' },
                 { from: 7, to: 8, color: '#C7AA1D' },
                 { from: 8, to: 10, color: '#98560A' },
             ],
-            tickPositions: [1.5, 2.5, 5, 7.5, 8.5], // 第一個 Y 軸的標籤位置
-            categories: ['A', 'B', 'C', 'D', 'E'], // 第一個 Y 軸的標籤
+            tickPositions: [1.5, 2.5, 5, 7.5, 8.5],
+            categories: ['A', 'B', 'C', 'D', 'E'],
             title: null,
             labels: {
                 formatter: function () {
                     let positionIndex = this.axis.tickPositions.indexOf(this.value);
-                    if (positionIndex === -1) {
-                        positionIndex = 0;
-                    }
+                    if (positionIndex === -1) positionIndex = 0;
                     return this.axis.categories[positionIndex] || '';
                 },
                 style: {
@@ -78,37 +67,15 @@ Highcharts.chart('containerCii', {
             },
             opposite: false,
         },
-        {
-            // 第二個 Y 軸
-            offset: 0,
-            plotBands: null,
-            tickPositions: [0, 1.2, 2.5, 7.5, 8.8, 10], // 第一個 Y 軸的標籤位置
-            categories: ['5', '5.4', '6.12', '6.96', '7.75', '8'], // 第一個 Y 軸的標籤
-            title: null,
-            labels: {
-                formatter: function () {
-                    let positionIndex = this.axis.tickPositions.indexOf(this.value);
-                    if (positionIndex === -1) {
-                        positionIndex = 0;
-                    }
-                    return this.axis.categories[positionIndex] || '';
-                },
-                style: {
-                    color: '#FFFFFF',
-                },
-            },
-            opposite: true,
-        },
     ],
     series: [
         {
             data: [
                 {
                     y: 100,
-                    target: 2, // 紅色BAR位置，依照百分比
+                    target: 2,
                 },
             ],
         },
     ],
-    tooltip: {},
 });
